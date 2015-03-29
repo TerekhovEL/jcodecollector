588ba9c133cb01729e0fdf78db1f9ac2429ad95f
Karl-Philipp Richter <krichter722@aol.de>
2015-03-29 18:34:39 +0200

    transformed array iteration

--- src/main/java/jcodecollector/io/FileManager.java
+++ src/main/java/jcodecollector/io/FileManager.java
@@ -22,11 +22,11 @@ public class FileManager {
     public static boolean deleteDirectory(File path) {
         if (path.exists()) {
             File[] files = path.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    deleteDirectory(files[i]);
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDirectory(file);
                 } else {
-                    files[i].delete();
+                    file.delete();
                 }
             }
         }
